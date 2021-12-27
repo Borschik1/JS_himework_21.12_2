3c9ea1303fb666bb06655659b6571e7eb28eb132
Borschik <47531847+Borschik1@users.noreply.github.com>
2021-12-27 15:41:26 +0500

    Update float.js

--- float.js
+++ float.js
@@ -1,54 +1,22 @@
-function float_num(x) {
-	if (x[0] == '-'){
-		sBit = "1";
-	} else {
-		sBit = "0";
+function float_num(num) {
+	if (num == 0) {
+		return "0".repeat(32);
 	}
-	x = Math.abs(x);
-	s = x.toString(2);
-	if (x == 0) {
-		return sBit + "0000000000000000000000000000000";
+	let sign = 1;
+	if (num > 0) {
+		let sign = 0;
 	}
-	if (x > ((2 - Math.pow(2, -23)) * Math.pow(2, 127))){
-		return "01111111100000000000000000000000";
-	}
-	dotIndex = -1;
-	digit_after_dot = -1;
-	for (i = 0; i < s.length; i++){
-		if (s[i] == '.'){
-			dotIndex = i;
-			break;
-		}
-	}
-	for (i = dotIndex; i < s.length; i++){
-		if (s[i] == '1') {
-			digit_after_dot = i;
-			break;
-		}
-	}
-	order = "";
-	mantiss = "";
-	if (Math.abs(x) >= 1){
-		if (dotIndex == -1){
-			order = (s.length + 126).toString(2);
-		} else {
-			order = (dotIndex + 126).toString(2);
-		}
-		mantiss = s.slice(1);
-	} else {
-		order = (dotIndex - digit_after_dot + 127).toString(2);
-		mantiss = s.slice(digit_after_dot + 1);
-	}
-	order = "0".repeat(8 - order.length) + order;
-	for (i = 0; i < mantiss.length; i++){
-		if (mantiss[i] == '.'){
-			mantiss = mantiss.slice(0, i) + mantiss.slice(i + 1);
-			break;
-		}
-	}
-	mantiss = mantiss + "0".repeat(23 - mantiss.length);
-	mantiss = mantiss.slice(0, 23);
-	return sBit + order + mantiss;
+    num = Math.abs(num);
+    let order = Math.floor(Math.log2(num));
+    num /=  Math.pow(2, order);
+    let m = num % 1;
+    let second = "";
+    for (let i = 0; i < 23; i++) {
+        m *= 2;
+        second += Math.floor(m);
+        m %= 1;
+    }
+    return sign + (127 + order).toString(2).padStart(8, "0") + second.padEnd(23, "0");
 }
 
 
